ddd707320b8ddc618062d74783cf94a371120d94
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-08-30 13:10:46 +0200

    commit before pull main

--- App.tsx
+++ App.tsx
@@ -2,25 +2,27 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Home from './pages/Home'
 
 const Stack = createNativeStackNavigator()
 
 const App: React.FC = () => {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Login'>
-				<Stack.Screen
-					name='Login'
-					component={Login}
-					options={{ title: 'Connexion' }}
-				/>
-				<Stack.Screen
-					name='Register'
-					component={Register}
-					options={{ title: 'Inscription' }}
-				/>
-			</Stack.Navigator>		
-		</NavigationContainer>
+		// <NavigationContainer>
+		// 	<Stack.Navigator initialRouteName='Login'>
+		// 		<Stack.Screen
+		// 			name='Login'
+		// 			component={Login}
+		// 			options={{ title: 'Connexion' }}
+		// 		/>
+		// 		<Stack.Screen
+		// 			name='Register'
+		// 			component={Register}
+		// 			options={{ title: 'Inscription' }}
+		// 		/>
+		// 	</Stack.Navigator>		
+		// </NavigationContainer>
+		<Home />
 	)
 }
 export default App
