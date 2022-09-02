e98ebec9aafb2bd862c1c887c032f6ab861da4f0
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-09-02 15:50:13 +0200

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,30 +1,149 @@
 import { useContext, useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, StatusBar, Text, ScrollView } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 import { AuthContext } from '../context/AuthContext'
 import { styles } from '../style'
 
-const Home: React.FC = () => {
-	const { auth } = useContext(AuthContext)
-	const [userInfos, setUserInfos] = useState<any>({})
+import { useFonts, Montserrat_400Regular } from '@expo-google-fonts/montserrat'
 
-	useEffect(() => {
-		const getUserInfos = async () => {
-			const options = {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${auth}`,
-				},
-			}
-			const res = await fetch('http://localhost:5000/api/v1/users/me', options)
-			const data = await res.json()
-			setUserInfos(data)
-		}
-		getUserInfos()
-	}, [])
+import { Pentagon, Circle, Trapezoid } from 'react-native-shape'
+
+const Home: React.FC = () => {
+
+	let [fontsLoaded] = useFonts({
+		Montserrat_400Regular,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+	// const { auth } = useContext(AuthContext)
+	// const [userInfos, setUserInfos] = useState<any>({})
+
+	// useEffect(() => {
+	// 	const getUserInfos = async () => {
+	// 		const options = {
+	// 			method: 'GET',
+	// 			headers: {
+	// 				'Content-Type': 'application/json',
+	// 				'Authorization': `Bearer ${auth}`,
+	// 			},
+	// 		}
+	// 		const res = await fetch('http://localhost:5000/api/v1/users/me', options)
+	// 		const data = await res.json()
+	// 		setUserInfos(data)
+	// 	}
+	// 	getUserInfos()
+	// }, [])
 
 	return (
-		<View>
+		<View style={styles.home_container}>
+			<LinearGradient
+
+				colors={['#8693AB', '#BDD4E7', '#8693AB']}>
+				<StatusBar
+					hidden={true}
+				/>
+				<Image
+					source={require('../assets/logo.png')}
+					style={styles.logoHome}
+				/>
+
+
+				<Text style={styles.helloUser}>Bonjour Paul !</Text>
+				<ScrollView>
+					<Text style={styles.parcLocate}>Venez nous retrouver</Text>
+					<View style={styles.shadowProp}>
+						<Image
+							style={[styles.mapLh, styles.shadowProp]}
+							source={require('../assets/lhMap.png')}
+						/>
+					</View>
+
+
+					<Text style={styles.models}>Location de véhicule 100% électrique</Text>
+
+					<View style={styles.offer_card}>
+
+						<View style={styles.car_model_container}>
+							<Image
+								style={styles.peugeot_logo}
+								source={require('../assets/peugeot.png')}
+							/>
+							<Text style={styles.offer_name}>Citadine</Text>
+							<Image
+								style={styles.peugeot_plug}
+								source={require('../assets/plug.png')}
+							/>
+						</View>
+						<Text style={styles.offer_model}>Peugeot e-208</Text>
+						<Text style={styles.offer_specs}>Electrique · Automatique · 5 places </Text>
+						<View style={styles.offer_price_container}>
+						<Text style={styles.offer_price}> 11 €</Text><Text style={styles.offer_hour}> /heure</Text>
+						</View>
+
+						<Image
+							style={styles.car_offer_citadine}
+							source={require('../assets/citadine.png')}
+						/>
+
+					</View>
+					<View style={styles.offer_card}>
+
+						<View style={styles.car_model_container}>
+							<Image
+								style={styles.peugeot_logo}
+								source={require('../assets/peugeot.png')}
+							/>
+							<Text style={styles.offer_name}>Suv</Text>
+							<Image
+								style={styles.peugeot_plug}
+								source={require('../assets/plug.png')}
+							/>
+						</View>
+						<Text style={styles.offer_model}>Peugeot e-2008</Text>
+						<Text style={styles.offer_specs}>Electrique · Automatique · 5 places </Text>
+						<View style={styles.offer_price_container}>
+						<Text style={styles.offer_price}> 11 €</Text><Text style={styles.offer_hour}> /heure</Text>
+						</View>
+
+						<Image
+							style={styles.car_offer_suv}
+							source={require('../assets/suv.png')}
+						/>
+
+					</View>
+					<View style={styles.offer_card}>
+
+						<View style={styles.car_model_container}>
+							<Image
+								style={styles.peugeot_logo}
+								source={require('../assets/peugeot.png')}
+							/>
+							<Text style={styles.offer_name}>Combispace</Text>
+							<Image
+								style={styles.peugeot_plug}
+								source={require('../assets/plug.png')}
+							/>
+						</View>
+						<Text style={styles.offer_model}>Peugeot e-Rifter</Text>
+						<Text style={styles.offer_specs}>Electrique · Automatique · 5 places </Text>
+						<View style={styles.offer_price_container}>
+						<Text style={styles.offer_price}> 11 €</Text><Text style={styles.offer_hour}> /heure</Text>
+						</View>
+
+						<Image
+							style={styles.car_offer_combispace}
+							source={require('../assets/combispace.png')}
+						/>
+					</View>
+
+
+				</ScrollView>
+
+			</LinearGradient>
 
 		</View>
 	)
--- style/index.tsx
+++ style/index.tsx
@@ -1,3 +1,13 @@
+// 	$electricBlue: #35DDF0;
+// $darkSilver: #8693AB;
+// $lightSilver: #BDD4E7;
+// $darkFont: #01030D;
+// $white: #EAEAEA;
+// $lightContrast: #F6F6F6;
+// $darkGray: #C3CBDC;
+// $lightGray: #EDF1F4;
+
+
 import { StyleSheet } from 'react-native'
 
 export const mainColor: string = '#35DDF0'
@@ -33,5 +43,123 @@ export const styles = StyleSheet.create({
 	error_msg: {
 		marginBottom: 16,
 		color: 'red',
+	},
+	home_container: {
+		flex: 1,
+	},
+
+
+	logoHome: {
+		alignSelf: 'center',
+		marginTop: 10
+	},
+	helloUser: {
+
+		alignSelf: 'center',
+		padding: 15,
+		fontFamily: 'Montserrat_400Regular'
+
+	},
+	parcLocate: {
+		alignSelf: 'center',
+		marginTop: 30,
+		fontFamily: 'Montserrat_400Regular'
+	},
+	mapLh: {
+		marginTop: 10,
+		width: 330,
+		height: 330,
+		borderRadius: 15,
+		alignSelf: 'center'
+	},
+	models: {
+		alignSelf: 'center',
+		fontFamily: 'Montserrat_400Regular',
+		marginTop: 20
+	},
+
+
+	offer_card: {
+
+		height: 200,
+		width: 330,
+		backgroundColor: '#EAEAEA',
+		marginTop: 30,
+		marginBottom: 10,
+		alignSelf: 'center',
+		borderRadius: 15,
+		transform: [{ skewX: "-8deg" }],
+		overflow: 'hidden'
+	},
+	shadowProp: {
+		shadowColor: "#01030D",
+		shadowOffset: {
+			width: 0,
+			height: 6,
+		},
+		shadowOpacity: 0.37,
+		shadowRadius: 7.49,
+		//For Android
+		//elevation: 12,
+	},
+	car_model_container: {
+		flexDirection: "row",
+		alignItems: "center"
+	},
+	offer_name: {
+		fontSize: 10,
+		marginTop: 19,
+		marginLeft: 10
+	},
+
+	car_offer_citadine: {
+		width: 190,
+		height: 120,
+		marginTop: -70,
+		marginLeft: 145,
+	},
+	car_offer_suv: {
+		width: 200,
+		height: 120,
+		marginTop: -65,
+		marginLeft: 135,
+	},
+	car_offer_combispace: {
+		width: 200,
+		height: 120,
+		marginTop: -70,
+		marginLeft: 140,
+	},
+	peugeot_logo: {
+		width: 25,
+		height: 25,
+		marginLeft: 15,
+		marginTop: 15
+	},
+	peugeot_plug: {
+		width: 18,
+		height: 18,
+		marginLeft: 10,
+		marginTop: 15
+	},
+	offer_model: {
+		marginLeft: 18,
+		marginTop: 10
+	},
+	offer_specs: {
+		fontSize: 7,
+		marginLeft: 18
+	},
+	offer_price_container:{
+		flexDirection: 'row',
+		alignItems:'baseline',
+		marginLeft: 5,
+		marginTop: 32
+	},
+	offer_price: {
+	},
+	offer_hour: {
+		fontSize: 8,
 	}
+
 })
